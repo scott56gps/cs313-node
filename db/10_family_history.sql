1f236a394fbae34657933ddab909cdc5a54997d8
Scott Nicholes <scott93gps@gmail.com>
2019-03-14 14:01:03 -0600

    Added birth_date and INSERT queries

--- db/10_family_history.sql
+++ db/10_family_history.sql
@@ -1,11 +1,36 @@
+DROP TABLE IF EXISTS parent_child, person;
+
 CREATE TABLE person (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
+    last_name VARCHAR(50) NOT NULL,
+    birth_date DATE
 );
 
 CREATE TABLE parent_child (
     id SERIAL PRIMARY KEY,
     parent_id INTEGER REFERENCES person(id) NOT NULL,
     child_id INTEGER REFERENCES person(id) NOT NULL
+);
+
+INSERT INTO person (first_name, last_name, birth_date) VALUES (
+    'Nellie',
+    'Nicholes',
+    11-DEC-1993
+), (
+    'Scott',
+    'Nicholes',
+    09-AUG-1993
+), (
+    'Echo',
+    'Nicholes',
+    13-JAN-2019
+);
+
+INSERT INTO parent_child (parent_id, child_id) VALUES (
+    1,
+    1
+), (
+    2,
+    1
 );

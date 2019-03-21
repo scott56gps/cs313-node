180e1f20c3b0de021a206ef88255a33ad1c9c551
Scott Nicholes <scott93gps@gmail.com>
2019-03-21 10:29:08 -0600

    Added Class Activity for Week 11 and rearranged calculator files

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Send in the Controllers!
+const calculatorController = require('./controllers/calculator-controller');
 const pieController = require('./controllers/pie-controller');
 const w10Controller = require('./controllers/w10-controller');
+const scripturesController = require('./controllers/scriptures-controller');
 
 // Set up Static File serving
 app.use(express.static('public'));
@@ -20,7 +22,8 @@ app.use(express.urlencoded({extended: true}));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.get('/calculate-rate', handleCalculateRate);
+app.get('/calculate-rate', calculatorController.handleCalculateRate);
+
 app.get('/pieTypes', pieController.getPieTypes);
 app.get('/pie/:id', pieController.handleGetPie);
 app.post('/pie', pieController.createPie);
@@ -28,103 +31,8 @@ app.post('/pie', pieController.createPie);
 // Week 10 Team Activity
 app.get('/getPerson/:id', w10Controller.handleGetPerson);
 
-function handleCalculateRate(request, response) {
-    var weight = request.query.weight;
-    var type = request.query.type;
-
-    // Calculate the rate based on the weight and type of mail
-    if (type == 'stampedLetters') {
-        rate = calculateStampedLetter(weight)
-        type = 'Letters (Stamped)';
-    } else if (type == 'meteredLetters') {
-        rate = calculateMeteredLetter(weight)
-        type = 'Letters (Metered)';
-    } else if (type == 'largeEnvelopes') {
-        rate = calculateLargeEnvelope(weight)
-        type = 'Large Envelopes (Flats)';
-    } else if (type == 'firstClassPackage') {
-        rate = calculateFirstClass(weight);
-        type = 'First-Class Package Service—Retail';
-    }
-
-    var params = {
-        weight: weight,
-        rate: rate,
-        type: type
-    }
-
-    response.render('results', params);
-}
-
-function calculateStampedLetter(weight) {
-    if (weight < 1) {
-        return 0.55
-    } else if (weight < 2) {
-        return 0.70
-    } else if (weight < 3) {
-        return 0.85
-    } else if (weight < 3.5) {
-        return 1.00
-    } else {
-        return calculateLargeEnvelope(weight)
-    }
-}
-
-function calculateMeteredLetter(weight) {
-    if (weight < 1) {
-        return 0.50
-    } else if (weight < 2) {
-        return 0.65
-    } else if (weight < 3) {
-        return 0.80
-    } else if (weight < 3.5) {
-        return 0.95
-    } else {
-        return calculateLargeEnvelope(weight)
-    }
-}
-
-function calculateLargeEnvelope(weight) {
-    if (weight < 1) {
-        return 1.00
-    } else if (weight < 2) {
-        return 1.15
-    } else if (weight < 3) {
-        return 1.30
-    } else if (weight < 4) {
-        return 1.45
-    } else if (weight < 5) {
-        return 1.45
-    } else if (weight < 6) {
-        return 1.45
-    } else if (weight < 7) {
-        return 1.45
-    } else if (weight < 8) {
-        return 1.45
-    } else if (weight < 9) {
-        return 1.45
-    } else if (weight < 10) {
-        return 1.45
-    } else if (weight < 11) {
-        return 1.45
-    } else if (weight < 12) {
-        return 1.45
-    } else if (weight < 13) {
-        return 1.45
-    }
-}
-
-function calculateFirstClass(weight) {
-    if (weight < 4) {
-        return 3.66
-    } else if (weight < 8) {
-        return 4.39
-    } else if (weight < 12) {
-        return 5.19
-    } else if (weight < 13) {
-        return 5.71
-    }
-}
+// Week 11 Team Activity - Scriptures
+app.get('/scriptures', scripturesController.hanldeGetScriptures);
 
 app.listen(port, function () {
     console.log('Server now listening on port ' + port);
